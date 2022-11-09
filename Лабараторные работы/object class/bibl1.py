dc202aef8c78e08eb94ee46c0a43a517fb4a5f67
Danya300 <74235043+Danya300@users.noreply.github.com>
2022-11-09 15:34:03 +0300

    Update bibl1.py

--- Лабараторные работы/object class/bibl1.py
+++ Лабараторные работы/object class/bibl1.py
@@ -203,3 +203,18 @@ print(Alex)
 Features = namedtuple('Features', ['age', 'gender', 'name'])
 row = Features(age=22, gender='male', name='Alex')
 print(row.gender)
+##Обобщенные классы в Python
+from typing import TypeVar, Generic, List
+
+T = TypeVar('T')
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        # Create an empty list with items of type T
+        self.items: List[T] = []
+
+    def push(self, item: T) -> None:
+        self.items.append(item)
+
+    def pop(self) -> T:
+        return self.items.pop()
